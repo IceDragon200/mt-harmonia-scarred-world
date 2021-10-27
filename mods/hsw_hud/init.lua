9f8b3d8628617082e3e68fbde76f312247ce852b
Corey Powell <mistdragon100@gmail.com>
2021-10-27 12:49:25 -0500

    hsw_hud: Stub hook into environment

--- mods/hsw_hud/init.lua
+++ mods/hsw_hud/init.lua
@@ -2,3 +2,11 @@
 -- HSW HUD
 --
 local mod = foundation.new_module("hsw_hud", "0.0.0")
+
+nokore.environment:register_on_player_env_change("hsw_hud:heat_and_humidity_watcher", function (player, changes)
+  if changes.heat then
+  end
+
+  if changes.humidity then
+  end
+end)
