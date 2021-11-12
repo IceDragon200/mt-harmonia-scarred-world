170a8eb60affe6974192d85034f036fce3e9fbbf
Corey Powell <mistdragon100@gmail.com>
2021-11-11 23:48:38 -0500

    hsw_recipes: Added missing version to module

--- mods/hsw_recipes/init.lua
+++ mods/hsw_recipes/init.lua
@@ -1,6 +1,6 @@
 --
 -- Centralized mod for ALL recipes used in HSW for every sub system.
 --
-local mod = foundation.new_module("hsw_recipes")
+local mod = foundation.new_module("hsw_recipes", "0.0.0")
 
 mod:require("recipes.lua")
