1d16368d708bfc2255899566169349a71b244a5d
Corey Powell <mistdragon100@gmail.com>
2022-08-18 13:02:48 -0500

    nokore_prelude: Setup default and metal sounds if not already set

--- mods/nokore_prelude/init.lua
+++ mods/nokore_prelude/init.lua
@@ -16,8 +16,13 @@ nokore.is_prelude = true
 -- determine if the node sounds registry was created
 if not nokore.node_sounds then
   -- if not, create it
-  minetest.log("info", "initializing nokore node sounds registry")
-  nokore.node_sounds = foundation.com.NodeSoundsRegistry:new()
+  minetest.log("info", "referencing foundation node sounds registry")
+  nokore.node_sounds = assert(foundation.com.node_sounds)
 end
 
 -- @const node_sounds: foundation.com.NodeSoundsRegistry
+
+-- register the "default" node sound set, usually just empty
+nokore.node_sounds:register_new("default", {})
+
+nokore.node_sounds:register_new("metal", {})
