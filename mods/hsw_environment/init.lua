8cd7627200b3310cc4415fd5949a7d4d4809f15b
Corey Powell <mistdragon100@gmail.com>
2022-08-29 12:29:54 -0500

    hsw_environment: Infrequent environment change updates

--- mods/hsw_environment/init.lua
+++ mods/hsw_environment/init.lua
@@ -7,6 +7,8 @@ local mod = foundation.new_module("hsw_environment", "0.1.0")
 local Vector3 = foundation.com.Vector3
 
 nokore.environment = {
+  m_elapsed = 0,
+  m_elapsed_since_update = 0,
   m_on_player_env_change_cbs = {},
 }
 
@@ -46,48 +48,56 @@ function nokore.environment:register_on_player_env_change(name, callback)
 end
 
 nokore.player_service:register_update("hsw_environment:players_update", function (players, dtime, player_assigns)
-  local assigns
-  local pos
-  local data
-  local meta
-  local last_biome_id
-  local last_heat
-  local last_humidity
-  local changes
+  local env = nokore.environment
+  env.m_elapsed = env.m_elapsed + dtime
+  env.m_elapsed_since_update = env.m_elapsed_since_update + dtime
 
-  for player_name, player in pairs(players) do
-    assigns = player_assigns[player_name]
-    pos = player:get_pos()
-    pos = Vector3.round(pos, pos)
+  if env.m_elapsed_since_update > 0.05 then
+    env.m_elapsed_since_update = 0
 
-    data = minetest.get_biome_data(pos)
+    local assigns
+    local pos
+    local data
+    local meta
+    local last_biome_id
+    local last_heat
+    local last_humidity
+    local changes
 
-    meta = player:get_meta()
+    for player_name, player in pairs(players) do
+      assigns = player_assigns[player_name]
+      pos = player:get_pos()
+      pos = Vector3.round(pos, pos)
 
-    last_biome_id = meta:get_int("hsw_last_biome_id")
-    last_heat = meta:get_float("hsw_last_heat")
-    last_humidity = meta:get_float("hsw_last_humidity")
+      data = minetest.get_biome_data(pos)
 
-    meta:set_int("hsw_last_biome_id", data.biome)
-    meta:set_float("hsw_last_heat", data.heat)
-    meta:set_float("hsw_last_humidity", data.humidity)
+      meta = player:get_meta()
 
-    changes = {}
+      last_biome_id = meta:get_int("hsw_last_biome_id")
+      last_heat = meta:get_float("hsw_last_heat")
+      last_humidity = meta:get_float("hsw_last_humidity")
 
-    if last_biome_id ~= data.biome then
-      changes.biome = data.biome
-    end
+      meta:set_int("hsw_last_biome_id", data.biome)
+      meta:set_float("hsw_last_heat", data.heat)
+      meta:set_float("hsw_last_humidity", data.humidity)
 
-    if last_heat ~= data.heat then
-      changes.heat = data.heat
-    end
+      changes = {}
 
-    if last_humidity ~= data.humidity then
-      changes.humidity = data.humidity
-    end
+      if last_biome_id ~= data.biome then
+        changes.biome = data.biome
+      end
 
-    if next(changes) then
-      nokore.environment:trigger_on_player_env_change(player, changes)
+      if last_heat ~= data.heat then
+        changes.heat = data.heat
+      end
+
+      if last_humidity ~= data.humidity then
+        changes.humidity = data.humidity
+      end
+
+      if next(changes) then
+        nokore.environment:trigger_on_player_env_change(player, changes)
+      end
     end
   end
 end)
