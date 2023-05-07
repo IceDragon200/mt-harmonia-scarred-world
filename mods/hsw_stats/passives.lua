af704252a2a4b1b1960b46e15777136a829337ce
Corey Powell <mistdragon100@gmail.com>
2023-05-07 12:30:15 -0500

    hsw_stats: Added some more passives (that haven't been implemented yet)

--- mods/hsw_stats/passives.lua
+++ mods/hsw_stats/passives.lua
@@ -2,8 +2,35 @@ local mod = assert(hsw_stats)
 
 local reg = harmonia.passive.registry
 
+reg:register_passive("regeneration", {
+  description = mod.S("Regeneration"),
+
+  update = function (_self, player, time, time_max, counter, dtime)
+    time = time - dtime
+    return time, counter
+  end,
+})
+
 reg:register_passive("poison", {
   description = mod.S("Poison"),
+
+  update = function (_self, player, time, time_max, counter, dtime)
+    time = time - dtime
+    return time, counter
+  end,
+})
+
+reg:register_passive("burn", {
+  description = mod.S("Burn"),
+
+  update = function (_self, player, time, time_max, counter, dtime)
+    time = time - dtime
+    return time, counter
+  end,
+})
+
+reg:register_passive("freeze", {
+  description = mod.S("Freeze"),
 
   update = function (_self, player, time, time_max, counter, dtime)
     time = time - dtime
