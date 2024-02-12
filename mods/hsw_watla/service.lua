540b9af408f5ba25d61c0c6a56440de14b1dc413
Corey Powell <mistdragon100@gmail.com>
2024-02-11 21:11:32 -0500

    hsw_watla: update_player/4 function instead of massive inline function

--- mods/hsw_watla/service.lua
+++ mods/hsw_watla/service.lua
@@ -1,22 +1,22 @@
--- @namespace hsw_watla
+--- @namespace hsw_watla
 local Vector3 = assert(foundation.com.Vector3, "expected vector3 module")
 local raycast = assert(minetest.raycast, "expected raycast function")
 
--- @class Service
+--- @class Service
 local Service = foundation.com.Class:extends("hsw_watla.Service")
 local ic = Service.instance_class
 
--- @type LookingAtContext: {
---   player: PlayerRef,
---   wielded_item_name?: String,
---   wielded_item?: ItemStack,
---   eye_pos: Vector3,
---   target_pos: Vector3,
---   targets?: {first: PointedThing, object?: PointedThing, node?: PointedThing},
--- }
--- @type LookingAtCallback: Function(context: LookingAtContext, dtime: Float): void
+--- @type LookingAtContext: {
+---   player: PlayerRef,
+---   wielded_item_name?: String,
+---   wielded_item?: ItemStack,
+---   eye_pos: Vector3,
+---   target_pos: Vector3,
+---   targets?: {first: PointedThing, object?: PointedThing, node?: PointedThing},
+--- }
+--- @type LookingAtCallback: Function(context: LookingAtContext, dtime: Float): void
 
--- @spec #initialize(): void
+--- @spec #initialize(): void
 function ic:initialize()
   self.default_look_range = 4
 
@@ -32,11 +32,11 @@ function ic:initialize()
   self.m_registered_context_mod_cbs = {}
 end
 
--- Registers a "Looking-At" callback, every frame this function will invoke the callbacks
--- with information on what a player is looking at, see the LookingAtCallback for more information.
--- It is up to the callee to handling any timing or focus changes.
---
--- @spec #register_looking_at(name: String, LookingAtCallback): void
+--- Registers a "Looking-At" callback, every frame this function will invoke the callbacks
+--- with information on what a player is looking at, see the LookingAtCallback for more information.
+--- It is up to the callee to handling any timing or focus changes.
+---
+--- @spec #register_looking_at(name: String, LookingAtCallback): void
 function ic:register_looking_at(name, callback)
   if self.m_registered_looking_at_cbs[name] then
     error("Looking At Callback already registered name=" .. name)
@@ -45,7 +45,7 @@ function ic:register_looking_at(name, callback)
   self.m_registered_looking_at_cbs[name] = callback
 end
 
--- @spec #register_context_mod(name: String, LookingAtCallback): void
+--- @spec #register_context_mod(name: String, LookingAtCallback): void
 function ic:register_context_mod(name, callback)
   if self.m_registered_context_mod_cbs[name] then
     error("Context Mod(ifier) Callback already registered name=" .. name)
@@ -54,21 +54,7 @@ function ic:register_context_mod(name, callback)
   self.m_registered_context_mod_cbs[name] = callback
 end
 
--- Batch processes the players and handles all the looking_at callbacks
---
--- @spec #update_players(
---   players: { [player_name: String]: PlayerRef },
---   dtime: Float,
---   player_assigns: Table,
---   trace: Trace
--- ): void
-function ic:update_players(players, dtime, _player_assigns, trace)
-  self.m_elapsed = self.m_elapsed + dtime
-  self.m_elapsed_since_update = self.m_elapsed_since_update + dtime
-  if self.m_elapsed_since_update < self.m_update_interval then
-    return
-  end
-
+function ic:update_player(player, dtime, assigns, span)
   local player_properties
   local pos
   local look_dir
@@ -80,89 +66,107 @@ function ic:update_players(players, dtime, _player_assigns, trace)
   local wielded_item
   local targets
   local context
-  local span
   local cb_span
+
+  player_properties = player:get_properties()
+  eye_height = assert(player_properties.eye_height, "expected eye height")
+  pos = player:get_pos()
+  pos.y = pos.y + eye_height
+  look_dir = player:get_look_dir()
+  look_range = self.default_look_range
+
+  wielded_item = player:get_wielded_item():get_definition()
+  wielded_item_name = nil
+  if wielded_item and wielded_item.range then
+    wielded_item_name = wielded_item.name
+    look_range = wielded_item.range
+  end
+
+  target_pos = Vector3.add(target_pos, pos, Vector3.multiply(tmp_v3, look_dir, look_range))
+
+  targets = {}
+
+  for pt in raycast(pos, target_pos, true, false) do
+    if pt.type == "object" and pt.ref ~= player and pt.ref:get_attach() ~= player then
+      -- if we haven't already populated the object, then do so
+      if not targets.object then
+        -- if the first pointed thing has not been set, then set it
+        if not targets.first then
+          targets.first = pt
+        end
+        targets.object = pt
+      end
+    elseif pt.type == "node" then
+      -- if we haven't already populated the node, then do so
+      if not targets.node then
+        -- if the first pointed thing has not been set, then set it
+        if not targets.first then
+          targets.first = pt
+        end
+        targets.node = pt
+      end
+    end
+
+    if targets.node and targets.object then
+      -- both targets have been filled, abort
+      break
+    end
+  end
+
+  context = {
+    player = player,
+    wielded_item_name = wielded_item_name,
+    wielded_item = wielded_item,
+    eye_pos = pos,
+    target_pos = target_pos,
+    targets = targets,
+  }
+
+  for callback_name, callback in pairs(self.m_registered_context_mod_cbs) do
+    if span then
+      cb_span = span:span_start(callback_name)
+    end
+    context = callback(context, self.m_elapsed_since_update, cb_span)
+    if cb_span then
+      cb_span:span_end()
+    end
+  end
+  cb_span = nil
+
+  for callback_name, callback in pairs(self.m_registered_looking_at_cbs) do
+    if span then
+      cb_span = span:span_start(callback_name)
+    end
+    callback(context, self.m_elapsed_since_update, cb_span)
+    if cb_span then
+      cb_span:span_end()
+    end
+  end
+  cb_span = nil
+end
+
+--- Batch processes the players and handles all the looking_at callbacks
+---
+--- @spec #update_players(
+---   players: { [player_name: String]: PlayerRef },
+---   dtime: Float,
+---   player_assigns: Table,
+---   trace: Trace
+--- ): void
+function ic:update_players(players, dtime, player_assigns, trace)
+  self.m_elapsed = self.m_elapsed + dtime
+  self.m_elapsed_since_update = self.m_elapsed_since_update + dtime
+  if self.m_elapsed_since_update < self.m_update_interval then
+    return
+  end
+
+  local span
 
   for player_name, player in pairs(players) do
     if trace then
       span = trace:span_start(player_name)
     end
-    player_properties = player:get_properties()
-    eye_height = assert(player_properties.eye_height, "expected eye height")
-    pos = player:get_pos()
-    pos.y = pos.y + eye_height
-    look_dir = player:get_look_dir()
-    look_range = self.default_look_range
-
-    wielded_item = player:get_wielded_item():get_definition()
-    wielded_item_name = nil
-    if wielded_item and wielded_item.range then
-      wielded_item_name = wielded_item.name
-      look_range = wielded_item.range
-    end
-
-    target_pos = Vector3.add(target_pos, pos, Vector3.multiply(tmp_v3, look_dir, look_range))
-
-    targets = {}
-
-    for pt in raycast(pos, target_pos, true, false) do
-      if pt.type == "object" and pt.ref ~= player and pt.ref:get_attach() ~= player then
-        -- if we haven't already populated the object, then do so
-        if not targets.object then
-          -- if the first pointed thing has not been set, then set it
-          if not targets.first then
-            targets.first = pt
-          end
-          targets.object = pt
-        end
-      elseif pt.type == "node" then
-        -- if we haven't already populated the node, then do so
-        if not targets.node then
-          -- if the first pointed thing has not been set, then set it
-          if not targets.first then
-            targets.first = pt
-          end
-          targets.node = pt
-        end
-      end
-
-      if targets.node and targets.object then
-        -- both targets have been filled, abort
-        break
-      end
-    end
-
-    context = {
-      player = player,
-      wielded_item_name = wielded_item_name,
-      wielded_item = wielded_item,
-      eye_pos = pos,
-      target_pos = target_pos,
-      targets = targets,
-    }
-
-    for callback_name, callback in pairs(self.m_registered_context_mod_cbs) do
-      if span then
-        cb_span = span:span_start(callback_name)
-      end
-      context = callback(context, self.m_elapsed_since_update, cb_span)
-      if cb_span then
-        cb_span:span_end()
-      end
-    end
-    cb_span = nil
-
-    for callback_name, callback in pairs(self.m_registered_looking_at_cbs) do
-      if span then
-        cb_span = span:span_start(callback_name)
-      end
-      callback(context, self.m_elapsed_since_update, cb_span)
-      if cb_span then
-        cb_span:span_end()
-      end
-    end
-    cb_span = nil
-
+    self:update_player(player, dtime, player_assigns[player_name])
     if span then
       span:span_end()
       span = nil
