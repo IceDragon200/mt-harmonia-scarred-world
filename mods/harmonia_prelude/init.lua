2c8a1286c1a011620ff0d6485bc5a222b8582622
Corey Powell <mistdragon100@gmail.com>
2025-03-08 20:05:10 -0500

    harmonia_prelude: Add new special config flag enable_mana_hud_integration

--- mods/harmonia_prelude/init.lua
+++ mods/harmonia_prelude/init.lua
@@ -1,1 +1,7 @@
 local mod = foundation.new_module("harmonia_prelude", "0.0.0")
+
+harmonia = rawget(_G, "harmonia") or {}
+harmonia.config = harmonia.config or {}
+
+--- Inform harmonia mana NOT to enable its own hud integrations, we'll do it in hsw_huds.
+harmonia.config.enable_mana_hud_integration = false
